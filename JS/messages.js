0f6006f3d0948d879fc4b82341d8de6fdf12ac1c
mohamedhamdy9966 <mh7008002@gmail.com>
2025-05-07 12:40:30 +0300

    customer-service-add

---
+++ JS/messages.js
@@ -0,0 +1,21 @@
+const messages = [
+  {
+    name: "mona",
+    email: "mona@iti.com",
+    message: "lorem lorem lorem lorem lorem",
+  },
+  {
+    name: "nasr",
+    email: "nasr@iti.com",
+    message:
+      "fjfkjklkjlj yyutyuuru yufrrutruy yruyguygu ,bbnmbmvvnb ytyutuuyg gsdsgdsd uyoiutyutuy utriytryt nmbmnvnbv",
+  },
+  {
+    name: "omar",
+    email: "omar@iti.com",
+    message:
+      "fjfkjklkjlj yyutyuuru yufrrutruy yruyguygu ,bbnmbmvvnb ytyutuuyg gsdsgdsd uyoiutyutuy utriytryt nmbmnvnbv",
+  },
+];
+
+localStorage.setItem("messages", JSON.stringify(messages));
